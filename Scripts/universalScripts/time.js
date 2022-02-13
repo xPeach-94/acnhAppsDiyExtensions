816b8e86a7bbc5c1aa3b5051c5a57d239660368b
xPeach-94 <sabrinahermans@live.nl>
2022-02-13 19:25:20 +0100

    fix update

--- Scripts/universalScripts/time.js
+++ Scripts/universalScripts/time.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 let dateObj = new Date();
 
 const myTimer = function (date) {
@@ -47,54 +46,4 @@ const getDate = function ()
 }
 
 let curDate = getYear() + "-" + getMonth() + "-" + getDate();
-=======
-let dateObj = new Date();
-
-const myTimer = function (date) {
-  var date = document.getElementById("date");
-  var time = document.getElementById("time");
-  date.innerHTML = new Date().toLocaleDateString();
-  time.innerHTML = new Date().toLocaleTimeString();
-}
-
-setInterval(myTimer, 100);
-
-const leadingZeros = function (number) {
-  if ((number.toString()).length < 2) {
-    number = "0" + number;
-  }
-  
-  return number.toString();
-}
-
-
-const getYear = function () {
-  let currentYear = dateObj.getFullYear();
-
-  // return "2022";
-  return currentYear;
-}
-
-const getMonth = function () 
-{
-  let currentMonth = dateObj.getMonth() + 1;
-
-  currentMonth = leadingZeros(currentMonth);
-
-  // return "04";
-  return currentMonth;
-}
-
-const getDate = function () 
-{
-  let currentDate = dateObj.getDate();
-
-  currentDate = leadingZeros(currentDate);
-
-  // return "18";
-  return currentDate;
-}
-
-let curDate = getYear() + "-" + getMonth() + "-" + getDate();
->>>>>>> Stashed changes
 // console.log("Today's Date", curDate);
